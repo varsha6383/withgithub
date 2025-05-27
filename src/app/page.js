b67f07e44d9628a34ef721a1d9d6fafaae85e36c
varsha6383 <varsha.zenthtech@gmail.com>
2025-05-27 11:25:49 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Page = () => {
   return (
-    <div>The next js project  will be connect to github</div>
+    <div>deploy with next js and github</div>
   )
 }
